76eee6dee26d7d19bf96c33e1b7a2edd6cd7206a
DylanK46 <ykainth.dylan@gmail.com>
2023-12-08 17:10:16 +0000

    WHAT DID I BREAK

--- packages/lovelace-functions/checkteam.js
+++ packages/lovelace-functions/checkteam.js
@@ -1,14 +1,14 @@
-exports.main = () => {
-    // check the jwt token    
-    checkJWT (token);
-}
+// exports.main = () => {
+//     // check the jwt token    
+//     checkJWT (token);
+// }
 
 
-function checkJWT (token) {
-    var jwt = require('jsonwebtoken');
-    // verify a token symmetric - synchronous
-    var decoded = jwt.verify(token, 'shhhhh');
-    console.log(decoded.foo) // bar
+// function checkJWT (token) {
+//     var jwt = require('jsonwebtoken');
+//     // verify a token symmetric - synchronous
+//     var decoded = jwt.verify(token, 'shhhhh');
+//     console.log(decoded.foo) // bar
 
-    return ;
-}
+//     return ;
+// }
